22c199373cf88befaf8baed471f04cb82187c7a3
Rodrigo Azevedo <rodrigozcma@gmail.com>
2020-09-21 01:16:40 -0300

    Adding first test

---
+++ src/__tests__/helpers.spec.ts
@@ -0,0 +1,13 @@
+import { isValidDate } from '@helpers/helps'
+
+describe('Is valid?', () => {
+    it('Value of date should be true', () => {
+      expect(isValidDate('10-12-2020')).toEqual(true)
+  })
+})
+
+describe('Is valid?', () => {
+  it('Value of date should be true', () => {
+    expect(isValidDate('10-13-2020')).toEqual(false)
+})
+})
